f13d1bb0ed6d9e7d0dbd3c23cae9855baac6756c
singhvi1 <singvi182003@gmail.com>
2026-01-08 18:27:59 +0530

    fix: Remove environment files and update BASE_URL logic for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 // src/utils/api.js
 import axios from "axios";
-// import { BASE_URL } from "../utils/constant";
+import { BASE_URL } from "../utils/constant";
 
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 console.log(BASE_URL)
 const api = axios.create({
   baseURL: BASE_URL,
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000/api/v1" : "/";
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000/api/v1" : "/api/v1";
 
 import {
   LayoutDashboard,
